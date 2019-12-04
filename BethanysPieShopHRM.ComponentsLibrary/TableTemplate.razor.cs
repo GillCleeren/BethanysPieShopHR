4caeac4ac303112f7f7e5e86b074d2690a57df9d
Roland Guijt <roland.guijt@gmail.com>
2019-12-04 14:02:48 +0100

    Upgrade to .NET Core 3.1

---
+++ BethanysPieShopHRM.ComponentsLibrary/TableTemplate.razor.cs
@@ -0,0 +1,10 @@
+﻿using BethanysPieShopHRM.Shared;
+
+namespace BethanysPieShopHRM.ComponentsLibrary
+{
+    public partial class TableTemplate<TItem>
+        where TItem: BenefitModel
+    {
+       
+    }
+}
